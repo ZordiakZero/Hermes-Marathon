8cfa79199424d2854a27832359168b223963f0a6
joelaaguirre12 <joelaaguirre12@gmail.com>
2023-04-19 21:53:01 -0600

    Established logic for changing resolution, needs to implemented

--- Assets/Scripts/Resolution.cs
+++ Assets/Scripts/Resolution.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Resolution : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -10,9 +11,21 @@ public class Resolution : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void AdjustResolution(float value)
+        {
+            if (resolutionDropdown.value == 0)
+            {
+                Screen.SetResolution(1420, 960);
+
+            }
+            else if (resolutionDropdown.value == 1)
+            {
+                Screen.SetResolution(1024, 800);
+            }
+            else if (resolutionDropdown.value == 2)
+            {
+                Screen.SetResolution(600, 400);
+            }
+        }
     }
 }
