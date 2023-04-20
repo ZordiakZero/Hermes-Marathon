de5df3a1cebdb18521a2cfcec92af66cdc7db47d
joelaaguirre12 <joelaaguirre12@gmail.com>
2023-04-19 22:18:19 -0600

    Logic established using temperature as a template, not implemented yet, see comment on start()

--- Assets/Scripts/Resolution.cs
+++ Assets/Scripts/Resolution.cs
@@ -1,31 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class Resolution : MonoBehaviour
 {
+    public TMPro.TMP_Dropdown resolutionDropdown;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    // If you uncomment this block and recompile, the dropdown WILL BREAK! AM WORKING ON A FIX STILL -Joel
+//    void Start()
+//    {
+//        AdjustTemperature(resolutionDropdown.value);
+//    }
 
     public void AdjustResolution(float value)
+    {
+        if (resolutionDropdown.value == 0)
         {
-            if (resolutionDropdown.value == 0)
-            {
-                Screen.SetResolution(1420, 960);
+            Screen.SetResolution(1420, 960, false);
 
-            }
-            else if (resolutionDropdown.value == 1)
-            {
-                Screen.SetResolution(1024, 800);
-            }
-            else if (resolutionDropdown.value == 2)
-            {
-                Screen.SetResolution(600, 400);
-            }
+        }
+        else if (resolutionDropdown.value == 1)
+        {
+            Screen.SetResolution(1024, 800, false);
+        }
+        else if (resolutionDropdown.value == 2)
+        {
+            Screen.SetResolution(600, 400, false);
         }
     }
 }
