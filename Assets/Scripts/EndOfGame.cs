d7c96ae846521e0596ed550dad090009474416fa
ForgeOfJS <joshsoto6738@gmail.com>
2023-05-02 02:37:00 -0600

    End of game functionality
    - End of game unlocks next level, last level resets locks.
    - End of game has a score minimum that can be altered.

--- Assets/Scripts/EndOfGame.cs
+++ Assets/Scripts/EndOfGame.cs
@@ -1,14 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndOfGame : MonoBehaviour
 {
+    [Range(1f, 6f)]
+    public float endOfGameDelay;
+    public int scoreNeeded = 20;
+
+    void Start() 
+    {
+        
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.transform.name == "Player")
         {
-            Debug.Log("End of the game!");
+            //Player Score is enough to win
+            if (other.gameObject.GetComponent<MailScore>().finalScore >= scoreNeeded)
+            {
+                Debug.Log("End of the game! Delaying for " + endOfGameDelay + " seconds!");
+                PlayerDataSaveObject loadedPlayerData = DataManager.LoadPlayerData();
+                string currentLevel = UnityEngine.SceneManagement.SceneManager.GetActiveScene().name;
+                char levelNum = currentLevel[currentLevel.Length - 1];
+                switch (levelNum)
+                {
+                    case '1':
+                        loadedPlayerData.MaxLevelUnlocked = 1;
+                        break;
+                    case '2':
+                        loadedPlayerData.MaxLevelUnlocked = 2;
+                        break;
+                    case '3':
+                        loadedPlayerData.MaxLevelUnlocked = 0;
+                        break;
+                    default:
+                        Debug.LogError("Invalid level index.");
+                        break;
+                }
+                DataManager.SavePlayerData(loadedPlayerData);
+                StartCoroutine(DelayEnd(endOfGameDelay));
+            }
+            //Otherwise notify player
+            else 
+            {
+                Debug.Log("Not enough points, " + scoreNeeded + " points needed.");
+            }
         }
+    }
+
+    IEnumerator DelayEnd(float delay) {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene("MainMenuScene");
     }
 }
